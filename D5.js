097a6cfee429559efa18596d73f3e8375f9fcb85
Francesco9500 <francescorg95@gmail.com>
2022-12-12 14:35:49 +0100

    modifihe progetto

--- D5.js
+++ D5.js
@@ -9,28 +9,32 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ["dog", "cat", "hamster", "redfish"];
-console.log(pets);
+//const pets = ["dog", "cat", "hamster", "redfish"];
+//for (i = 0; i < pets.length; i++) {
+//  console.log(pets[i]);
+//}
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
-pets.sort();
-console.log(pets);
+//pets.sort();
+//console.log(pets);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
-pets.reverse();
-console.log(pets);
+//pets.reverse();
+//console.log(pets);
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
 
-pets.shift();
-console.log(pets);
+//pets.shift();
+//pets.push("dog");
+//console.log(pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -56,15 +60,27 @@ const cars = [
   },
 ];
 
+//for (i = 0; i < cars.length; i++) {
+//  cars[i].licensePlate = 5;
+//}
+//console.log(cars);
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
 
+//for (i = 0; i < cars.length; i++) {
+//  cars[i].trims.pop();
+//}
+//console.log(cars);
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
+//for (i = 0; i < cars.length; i++) {
+//  cars[i].justTrims;
+//}
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
@@ -75,6 +91,15 @@ const justTrims = [];
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+
+while (i < 13) {
+  console.log(numericArray[i]);
+  i++;
+  if (numericArray[i] === 32) {
+    console.log("Ciao");
+    break;
+  }
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
